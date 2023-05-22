d92e10837f8d002d9362c56bf4eefdf48cb6aa12
pyking004 <pyking004@gmail.com>
2023-05-22 14:59:20 +0530

    login completed

--- app1/urls.py
+++ app1/urls.py
@@ -6,4 +6,6 @@ app_name = 'app1'
 urlpatterns = [
     path('',views.app1,name='app1'),
     path('signup',views.sign_up,name='signup'),
+    path('login',views.userlogin,name='login'),
 ]
+
--- app1/views.py
+++ app1/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 # from django.contrib.auth.forms import UserCreationForm
 from app1.forms import SignupForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.form 
+
+def userlogin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data['username']
+            pwd = form.cleaned_data['password']
+            user = authenticate(username=uname,password=pwd)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect('/')
+    else:
+        form = AuthenticationForm()
+    context = {'form':form}
+    return render(request,'app1/login.html',context)
 
 def sign_up(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            # form = SignupForm()
+            form = SignupForm()
             messages.success(request,'Account Created successfully!!')
     else:
         form = SignupForm()
