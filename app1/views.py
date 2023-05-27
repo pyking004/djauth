d6d298160bddb4119aff363be72e645f3b45ae4f
pyking004 <pyking004@gmail.com>
2023-05-27 15:15:28 +0530

    profile done with authenticated

--- app1/views.py
+++ app1/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render,HttpResponseRedirect
 from app1.forms import SignupForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login,logout,get_user
+
 # Create your views here.form 
 
 def userlogout(request):
@@ -11,24 +12,30 @@ def userlogout(request):
     return HttpResponseRedirect('/app1/login')
 
 def profile(request):
-    context = {'msg':'this is profile page','name':request.user.name}
-    return render(request,'app1/profile.html',context)
+    if request.user.is_authenticated:
+        context = {'msg':'this is profile page','name':request.user.username}
+        return render(request,'app1/profile.html',context)
+    else:
+        return HttpResponseRedirect('/app1/login')
 
 def userlogin(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request,data=request.POST)
-        if form.is_valid():
-            uname = form.cleaned_data['username']
-            pwd = form.cleaned_data['password']
-            user = authenticate(username=uname,password=pwd)
-            if user is not None:
-                login(request,user)
-                messages.success(request,'Login Done !!')
-                return HttpResponseRedirect('/app1/profile')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                pwd = form.cleaned_data['password']
+                user = authenticate(username=uname,password=pwd)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Login Done !!')
+                    return HttpResponseRedirect('/app1/profile')
+        else:
+            form = AuthenticationForm()
+        context = {'form':form,'name':request.user.username}
+        return render(request,'app1/login.html',context)
     else:
-        form = AuthenticationForm()
-    context = {'form':form}
-    return render(request,'app1/login.html',context)
+        return HttpResponseRedirect('/app1/profile')
 
 def sign_up(request):
     if request.method == 'POST':
@@ -43,7 +50,7 @@ def sign_up(request):
     return render(request,'app1/signup.html',context)
 
 def index(request):
-    context = {'msg':'This is Index Page'}
+    context = {'msg':'This is Index Page','name':request.user.username}
     return render(request,'app1/index.html',context)
 
 
